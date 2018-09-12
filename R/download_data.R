53d1fee7dd923a125df0ed54d25f2e43180c5c52
lcgodoy <lucasdac.godoy@gmail.com>
2018-09-12 16:16:19 -0300

    function donwload_transf

--- R/download_data.R
+++ R/download_data.R
@@ -41,14 +41,23 @@ download_orcamento <- function(year = NULL, ...) {
 
 #' Download Licitacoes e Compras
 #'
+#' @description This functions is not ready for use.
+#'
 #' @param year \code{integer} between 2014 and 2018
 #' @param month \code{integer} between 1 and 12
 #' @param type \code{character} must be 'licitacoes' or 'compras'
+#' @param opt \code{character} there are three data frames in 'licitacoes' and 'compras',
+#' this parameter controls which one do you want to download. If \code{NULL} (default)
+#' then this three data frames will be merged. The options are 'Item', 'Compras', 'Licitacoes',
+#' 'Termo' and 'Participantes'. Note that, 'Licitacoes' and 'Participantes' are valid only if
+#' \code{type} = 'licitacoes'. 'Item' and \code{NULL} are the options allowd for both types.
 #' @param ... additional parameters
 #'
 #' @return \code{data.frame}
 #' @export
-download_lic_cont <- function(year = NULL, month = NULL, type = 'licitacoes', ...) {
+download_lic_cont <- function(year = NULL, month = NULL, type = 'licitacoes', opt = NULL, ...) {
+  stop('This functions is not ready to use.')
+
   if(any(!is.numeric(year)) | any(!year %in% 2014:as.numeric(format(Sys.Date(), '%Y'))))
     stop('Year must be integer between 2014 and 2018.')
 
@@ -71,12 +80,14 @@ download_lic_cont <- function(year = NULL, month = NULL, type = 'licitacoes', ..
 
   for(i in year) {
     for(j in month) {
-      file_name <- paste0(sprintf('%s_%d-%s', type, i, j), '.zip')
+      file_name <- paste0(sprintf('%s_%d%s', type, i, j), '.zip')
       dest <- paste(temp_dir, file_name, sep = '/')
       file.create(dest)
       utils::download.file(url = sprintf(link, type, i, j), destfile = dest, quiet = T, method = 'wget')
       # closeAllConnections()
-      utils::unzip(zipfile = dest, exdir = temp_dir, list = T)
+      if(is.null(opt)) {
+        utils::unzip(zipfile = dest, exdir = temp_dir, list = T)
+      }
     }
   }
 
@@ -87,6 +98,62 @@ download_lic_cont <- function(year = NULL, month = NULL, type = 'licitacoes', ..
                     as.character()
                   output <- suppressWarnings(data.table::fread(x, dec = ',', sep = ';'))
                   colnames(output) <- iconv(colnames(output), from = encoding, to = 'ASCII//TRANSLIT')
+                  output
+                }) %>% data.table::rbindlist()
+
+  unlink(list.files(temp_dir, full.names = T), recursive = T)
+
+  return(out)
+}
+
+#' Download Transferencia
+#'
+#' @description This functions is not ready for use.
+#'
+#' @param year \code{integer} between 2013 and 2018
+#' @param month \code{integer} between 1 and 12
+#' @param ... additional parameters
+#'
+#' @return \code{data.frame}
+#' @export
+download_transf <- function(year = NULL, month = NULL, ...) {
+  if(any(!is.numeric(year)) | any(!year %in% 2013:as.numeric(format(Sys.Date(), '%Y'))))
+    stop('Year must be integer between 2013 and 2018.')
+
+  if(any(!is.numeric(month)) | any(!month %in% 1:12))
+    stop('Month must be integer between 1 and 12.')
+
+  if(month < 10)
+    month <- paste0('0', month)
+
+  temp_dir <- tempdir(check = T)
+
+  link <- ('http://www.portaltransparencia.gov.br/download-de-dados/transferencias/%d%s')
+
+  lapply(list.files(path = temp_dir, pattern = '.csv$', full.names = T),
+         file.remove) %>% invisible()
+
+  for(i in year) {
+    for(j in month) {
+      file_name <- paste0(sprintf('transferencias_%d%s', i, j), '.zip')
+      dest <- paste(temp_dir, file_name, sep = '/')
+      file.create(dest)
+      utils::download.file(url = sprintf(link, i, j), destfile = dest, quiet = T, method = 'wget')
+      # closeAllConnections()
+      utils::unzip(zipfile = dest, exdir = temp_dir)
+    }
+  }
+
+  out <- lapply(list.files(path = temp_dir, pattern = '.csv$', full.names = T),
+                function(x) {
+                  # encoding <- readr::guess_encoding(file = x,
+                  #                                   n_max = -1)[1,1] %>%
+                  #   as.character()
+                  readLines(x) %>%
+                    iconv(from = 'ISO-8859-1', to = 'ASCII//TRANSLIT') %>%
+                    writeLines(con = x)
+                  output <- suppressWarnings(data.table::fread(x, dec = ',', sep = ';'))
+                  # colnames(output) <- iconv(colnames(output), )
                   output
                 }) %>% data.table::rbindlist()
 
