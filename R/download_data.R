054153ca413e3115076814285dea6f4035462614
lcgodoy <lucasdac.godoy@gmail.com>
2018-09-12 17:08:37 -0300

    functions to download data

--- R/download_data.R
+++ R/download_data.R
@@ -67,8 +67,10 @@ download_lic_cont <- function(year = NULL, month = NULL, type = 'licitacoes', op
   if(!type %in% c('licitacao', 'compras'))
     stop("c('licitacao', 'compras') are the only valid types")
 
-  if(month < 10)
-    month <- paste0('0', month)
+  for(i in seq_along(month)) {
+    if(month[i] < 10)
+      month[i] <- paste0('0', month[i])
+  }
 
   temp_dir <- tempdir(check = T)
 
@@ -108,8 +110,6 @@ download_lic_cont <- function(year = NULL, month = NULL, type = 'licitacoes', op
 
 #' Download Transferencia
 #'
-#' @description This functions is not ready for use.
-#'
 #' @param year \code{integer} between 2013 and 2018
 #' @param month \code{integer} between 1 and 12
 #' @param ... additional parameters
@@ -123,8 +123,12 @@ download_transf <- function(year = NULL, month = NULL, ...) {
   if(any(!is.numeric(month)) | any(!month %in% 1:12))
     stop('Month must be integer between 1 and 12.')
 
-  if(month < 10)
-    month <- paste0('0', month)
+  for(i in seq_along(month)) {
+    if(as.integer(month[i]) < 10) {
+      month[i] <- paste0('0', month[i])
+    }
+  }
+
 
   temp_dir <- tempdir(check = T)
 
@@ -143,6 +147,174 @@ download_transf <- function(year = NULL, month = NULL, ...) {
       utils::unzip(zipfile = dest, exdir = temp_dir)
     }
   }
+
+  out <- lapply(list.files(path = temp_dir, pattern = '.csv$', full.names = T),
+                function(x) {
+                  # encoding <- readr::guess_encoding(file = x,
+                  #                                   n_max = -1)[1,1] %>%
+                  #   as.character()
+                  readLines(x) %>%
+                    iconv(from = 'ISO-8859-1', to = 'ASCII//TRANSLIT') %>%
+                    writeLines(con = x)
+                  output <- suppressWarnings(data.table::fread(x, dec = ',', sep = ';'))
+                  # colnames(output) <- iconv(colnames(output), )
+                  output
+                }) %>% data.table::rbindlist()
+
+  unlink(list.files(temp_dir, full.names = T), recursive = T)
+
+  return(out)
+}
+
+#' Download Execucao de Despesas
+#'
+#' @param year \code{integer} between 2013 and 2018
+#' @param month \code{integer} between 1 and 12
+#' @param ... additional parameters
+#'
+#' @return \code{data.frame}
+#' @export
+download_exec_desp <- function(year = NULL, month = NULL, ...) {
+  if(any(!is.numeric(year)) | any(!year %in% 2014:as.numeric(format(Sys.Date(), '%Y'))))
+    stop('Year must be integer between 2014 and 2018.')
+
+  if(any(!is.numeric(month)) | any(!month %in% 1:12))
+    stop('Month must be integer between 1 and 12.')
+
+  for(i in seq_along(month)) {
+    if(as.integer(month[i]) < 10) {
+      month[i] <- paste0('0', month[i])
+    }
+  }
+
+  temp_dir <- tempdir(check = T)
+
+  link <- ('http://www.portaltransparencia.gov.br/download-de-dados/despesas-execucao/%d%s')
+
+  lapply(list.files(path = temp_dir, pattern = '.csv$', full.names = T),
+         file.remove) %>% invisible()
+
+  for(i in year) {
+    for(j in month) {
+      file_name <- paste0(sprintf('transferencias_%d%s', i, j), '.zip')
+      dest <- paste(temp_dir, file_name, sep = '/')
+      file.create(dest)
+      utils::download.file(url = sprintf(link, i, j), destfile = dest, quiet = T, method = 'wget')
+      # closeAllConnections()
+      utils::unzip(zipfile = dest, exdir = temp_dir)
+    }
+  }
+
+  out <- lapply(list.files(path = temp_dir, pattern = '.csv$', full.names = T),
+                function(x) {
+                  # encoding <- readr::guess_encoding(file = x,
+                  #                                   n_max = -1)[1,1] %>%
+                  #   as.character()
+                  readLines(x) %>%
+                    iconv(from = 'ISO-8859-1', to = 'ASCII//TRANSLIT') %>%
+                    writeLines(con = x)
+                  output <- suppressWarnings(data.table::fread(x, dec = ',', sep = ';'))
+                  # colnames(output) <- iconv(colnames(output), )
+                  output
+                }) %>% data.table::rbindlist()
+
+  unlink(list.files(temp_dir, full.names = T), recursive = T)
+
+  return(out)
+}
+
+#' Download Cartoes de Pagamentos
+#'
+#' @param year \code{integer} between 2013 and 2018
+#' @param month \code{integer} between 1 and 12
+#' @param type must be 'cpgf' (cartao de pagamentos do governo federal),
+#' 'cpcc' (cartao de pagamentos do governo federal - compras centralizadas)
+#' or 'cpdc' (cartao de pagamentos da defesa civil)
+#' @param ... additional parameters
+#'
+#' @return \code{data.frame}
+#' @export
+download_cp <- function(year = NULL, month = NULL, type = NULL, ...) {
+  if(any(!is.numeric(year)) | any(!year %in% 2014:as.numeric(format(Sys.Date(), '%Y'))))
+    stop('Year must be integer between 2014 and 2018.')
+
+  if(any(!is.numeric(month)) | any(!month %in% 1:12))
+    stop('Month must be integer between 1 and 12.')
+
+  if(length(type) > 1)
+    stop('You must provide just one type.')
+
+  if(! type %in% c('cpgf', 'cpcc', 'cpdc'))
+    stop('Type must be cpgf, cpcc or cpdc.')
+
+  for(i in seq_along(month)) {
+    if(as.integer(month[i]) < 10) {
+      month[i] <- paste0('0', month[i])
+    }
+  }
+
+  temp_dir <- tempdir(check = T)
+
+  link <- ('http://www.portaltransparencia.gov.br/download-de-dados/%s/%d%s')
+
+  lapply(list.files(path = temp_dir, pattern = '.csv$', full.names = T),
+         file.remove) %>% invisible()
+
+  for(i in year) {
+    for(j in month) {
+      file_name <- paste0(sprintf('%s_%d%s', type, i, j), '.zip')
+      dest <- paste(temp_dir, file_name, sep = '/')
+      file.create(dest)
+      utils::download.file(url = sprintf(link, type, i, j), destfile = dest, quiet = T, method = 'wget')
+      # closeAllConnections()
+      utils::unzip(zipfile = dest, exdir = temp_dir)
+    }
+  }
+
+  out <- lapply(list.files(path = temp_dir, pattern = '.csv$', full.names = T),
+                function(x) {
+                  # encoding <- readr::guess_encoding(file = x,
+                  #                                   n_max = -1)[1,1] %>%
+                  #   as.character()
+                  readLines(x) %>%
+                    iconv(from = 'ISO-8859-1', to = 'ASCII//TRANSLIT') %>%
+                    writeLines(con = x)
+                  output <- suppressWarnings(data.table::fread(x, dec = ',', sep = ';'))
+                  # colnames(output) <- iconv(colnames(output), )
+                  output
+                }) %>% data.table::rbindlist()
+
+  unlink(list.files(temp_dir, full.names = T), recursive = T)
+
+  return(out)
+}
+
+#' Download Receitas
+#'
+#' @param year \code{integer} between 2013 and 2018
+#' @param ... additional parameters
+#'
+#' @return \code{data.frame}
+#' @export
+download_receitas <- function(year = NULL, ...) {
+  if(any(!is.numeric(year)) | any(!year %in% 2013:as.numeric(format(Sys.Date(), '%Y'))))
+    stop('Year must be integer between 2013 and 2018.')
+
+  temp_dir <- tempdir(check = T)
+
+  link <- ('http://www.portaltransparencia.gov.br/download-de-dados/receitas/%d')
+
+  lapply(list.files(path = temp_dir, pattern = '.csv$', full.names = T),
+         file.remove) %>% invisible()
+
+  for(i in year) {
+      file_name <- paste0(sprintf('receitas_%d', i), '.zip')
+      dest <- paste(temp_dir, file_name, sep = '/')
+      file.create(dest)
+      utils::download.file(url = sprintf(link, i), destfile = dest, quiet = T, method = 'wget')
+      # closeAllConnections()
+      utils::unzip(zipfile = dest, exdir = temp_dir)
+     }
 
   out <- lapply(list.files(path = temp_dir, pattern = '.csv$', full.names = T),
                 function(x) {
